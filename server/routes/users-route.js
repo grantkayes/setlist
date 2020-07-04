1d019bc8e68582333808b978dcf500bb43b00d04
Grant Kayes <glk25@georgetown.edu>
2020-07-03 20:43:58 -0700

    started working on login route

--- server/routes/users-route.js
+++ server/routes/users-route.js
@@ -13,13 +13,7 @@ const router = express.Router()
 // i.e.: '/all' will become '/users/all'
 router.get('/all', usersController.usersGetAll)
 
-router.get('/:id', (req, res, next) => {
-  db.query('SELECT * FROM users WHERE id = $1', [req.params.id], (err, res) => {
-    if (err) {
-      return next(err)
-    }
-    res.send(res.rows[0])
-  })
-})
+router.get('/login', usersController.login)
+
 // Export router
 module.exports = router
